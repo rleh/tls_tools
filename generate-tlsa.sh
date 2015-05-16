4b6735197c7c4aca70011943a70e7113dbcc517e
Raphael Lehmann <raphael@rleh.de>
2015-05-16 19:10:13 +0200

    tlsa generator: default port 443

--- generate-tlsa.sh
+++ generate-tlsa.sh
@@ -11,8 +11,10 @@ TLSA_ALL=''
 for SERVER in "$@"
 do
 	HOST=$(echo $SERVER | cut -f1 -d:)
-	PORT=$(echo $SERVER | cut -f2 -d:)
-	if [ -z "$PORT" ]; then
+	if [[ $SERVER == *":"* ]]
+	then
+		PORT=$(echo $SERVER | cut -f2 -d:)
+	else
 		PORT="443"
 	fi
 	SHA256HASH=$(openssl s_client -connect $HOST:$PORT -servername $HOST -showcerts </dev/null 2>/dev/null | openssl x509 -noout -fingerprint -sha256 | tr -d : | cut -c 20-)
